5ef28ec20630e940b49316f7ae5a3456fe68dd2a
oshuster <shuster.oleksandr@gmail.com>
2024-08-30 00:29:51 +0300

    change CORS settings

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan("tiny"));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(express.json());
 
 app.use("/api/proxy", proxyRouter);
