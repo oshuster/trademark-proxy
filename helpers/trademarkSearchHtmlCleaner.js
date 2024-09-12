ff01d62b5361405cc228d919ef153c35cb4544ef
oshuster <shuster.oleksandr@gmail.com>
2024-09-12 21:42:28 +0300

    [!] change parsing tm-thumb

--- helpers/trademarkSearchHtmlCleaner.js
+++ helpers/trademarkSearchHtmlCleaner.js
@@ -26,11 +26,39 @@ export const trademarkSearchHtmlCleaner = (body) => {
     $(element).attr("src", `https://iprop-ua.com${src}`);
   });
 
-  $('a[href^="/"]').each((_, element) => {
-    const href = $(element).attr("href");
+  // Оновлюємо кожен div.tm-thumb
+  $("div.tm-thumb").each((_, element) => {
+    const $thumb = $(element);
+    const $link = $thumb.find('a[href^="/"]');
 
-    $(element).attr("href", `${URL_HREF}${href}`);
-    $(element).attr("data-id", `${href}`);
+    if ($link.length) {
+      let href = $link.attr("href"); // Отримуємо href з <a>
+      if (href) {
+        href = href.slice(1); // Видаляємо перший символ (слеш)
+      }
+      console.log(href);
+      const text = $link.text(); // Отримуємо текст з <a>
+      const $img = $link.find("img"); // Знаходимо <img> всередині <a>
+
+      // Додаємо атрибут data-id до div.tm-thumb без початкового слеша
+      if (href) {
+        $thumb.attr("data-id", href);
+      }
+
+      // Створюємо новий <span class="tm-id"> з текстом
+      const $span = $(`<span class="tm-id">${text}</span>`);
+
+      // Якщо є <img>, зберігаємо його поза <a>
+      if ($img.length) {
+        // Видаляємо <img> з <a>, але залишаємо в структурі
+        $link.empty();
+        // Вставляємо зображення перед span
+        $img.insertBefore($span);
+      }
+
+      // Замінюємо <a> на нові елементи: <img> і <span class="tm-id">
+      $link.replaceWith($img.add($span)); // Додаємо зображення і span одночасно
+    }
   });
 
   return {
